07c0a1612fd31a7a39b1ecbe155905907d4a87ce
Derek Jobst <djobst@palantir.com>
2021-06-03 18:28:27 +0000

    Fix oh-my-zsh install

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 sudo usermod --shell /usr/bin/zsh coder
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+if [[ ! -d "/home/coder/.oh-my-zsh" ]]; then
+	echo "Installing ohmyzsh"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
